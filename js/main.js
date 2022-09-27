edeb7d2c53c3b547c521516172799026fbf5d60d
Ahmed Hawam <ahmed_hawam1998@outlook.com>
2022-09-27 16:25:07 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+//=========== Active navbar =============
+const currentLocation = location.href;
+const menuItem = document.querySelectorAll(
+  ".navbar .nav-item .nav-link"
+);
+const menuLength = menuItem.length;
+for (let i = 0; i < menuLength; i++) {
+  if (menuItem[i].href === currentLocation) {
+    menuItem[i].className = "nav-link active";
+  }
+}
+//=========== Active navbar =============
+
+//=========== Close inspect =============
+// document.addEventListener("contextmenu", (event) => event.preventDefault());
+// document.onkeydown = function (e) {
+//   if (event.keyCode == 123) {
+//     return false;
+//   }
+//   if (e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) {
+//     return false;
+//   }
+//   if (e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)) {
+//     return false;
+//   }
+//   if (e.ctrlKey && e.keyCode == "U".charCodeAt(0)) {
+//     return false;
+//   }
+// };
+//=========== Close inspect =============
+
+
+//=========== Bluring window  =============
+// var noPrint = true;
+// var noCopy = true;
+// var noScreenshot = true;
+// var autoBlur = true;
+//=========== Bluring window  =============
