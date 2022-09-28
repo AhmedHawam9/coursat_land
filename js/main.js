d9c9ea7106962e53eb0633cfd5397ad5e4229732
Ahmed Hawam <ahmed_hawam1998@outlook.com>
2022-09-28 14:21:42 +0200

    main

--- js/main.js
+++ js/main.js
@@ -16,37 +16,6 @@ var noScreenshot = true;
 var autoBlur = true;
 //=========== Bluring window  =============
 
-// document.addEventListener("keyup", function (e) {
-//   var keyCode = e.keyCode ? e.keyCode : e.which;
-//   if (keyCode == 44) {
-//     stopPrntScr();
-//   }
-// });
-// function stopPrntScr() {
-//   var inpFld = document.createElement("input");
-//   inpFld.setAttribute("value", ".");
-//   inpFld.setAttribute("width", "0");
-//   inpFld.style.height = "0px";
-//   inpFld.style.width = "0px";
-//   inpFld.style.border = "0px";
-//   document.body.appendChild(inpFld);
-//   inpFld.select();
-//   document.execCommand("copy");
-//   inpFld.remove(inpFld);
-// }
-// function AccessClipboardData() {
-//   try {
-//     window.clipboardData.setData("text", "Access   Restricted");
-//   } catch (err) {}
-// }
-// setInterval("AccessClipboardData()", 300);
-
-// Disable all button in keyboard
-// document.onkeydown = function (e) {
-//   return false;
-// };
-// Disable all button in keyboard
-
 /*
 NoPrint.js V1.0
 Created by PDFAntiCopy.com
@@ -163,7 +132,51 @@ document.onkeydown = function (e) {
     return false;
   }
 };
-document.addEventListener('keypress', () => {
+document.addEventListener("keypress", () => {
   toBlur();
 });
 //=========== Close inspect =============
+
+// an alternative structure to check individual matches
+if (
+  navigator.userAgent.match(/Android/i) ||
+  navigator.userAgent.match(/iPhone/i)
+  ) {
+  console.log("test")
+  // toBlur();
+  // making individual checks
+}
+function onReady() {
+  window.open("https://www.google.com.au","myWindow","dialog=yes,resizable=no");
+}
+
+// document.addEventListener("keyup", function (e) {
+//   var keyCode = e.keyCode ? e.keyCode : e.which;
+//   if (keyCode == 44) {
+//     stopPrntScr();
+//   }
+// });
+// function stopPrntScr() {
+//   var inpFld = document.createElement("input");
+//   inpFld.setAttribute("value", ".");
+//   inpFld.setAttribute("width", "0");
+//   inpFld.style.height = "0px";
+//   inpFld.style.width = "0px";
+//   inpFld.style.border = "0px";
+//   document.body.appendChild(inpFld);
+//   inpFld.select();
+//   document.execCommand("copy");
+//   inpFld.remove(inpFld);
+// }
+// function AccessClipboardData() {
+//   try {
+//     window.clipboardData.setData("text", "Access   Restricted");
+//   } catch (err) {}
+// }
+// setInterval("AccessClipboardData()", 300);
+
+// Disable all button in keyboard
+// document.onkeydown = function (e) {
+//   return false;
+// };
+// Disable all button in keyboard
